748dc9654b0f28ee71ed40165e4c02db49f5a1ca
Adas <100019532+adasjusk@users.noreply.github.com>
2025-03-16 21:05:17 +0200

    Create bostr.bat

---
+++ bostr.bat
@@ -0,0 +1,9 @@
+@echo off
+echo --------------------------------------------------
+echo  Hello and welcome so this program is rewriten
+echo  in python and no longer works the normal 
+echo  batch please migrate to new python based app
+echo  and there will be a legacy branch for archive.  
+echo ---------------------------------------------------
+pause
+exit
