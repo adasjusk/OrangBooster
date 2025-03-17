72e908a648c863b6e651a7957ccd1707c097a737
Adas <100019532+adasjusk@users.noreply.github.com>
2025-03-17 17:14:06 +0200

    Update bostr.bat

--- bostr.bat
+++ bostr.bat
@@ -1,10 +1,10 @@
 @echo off
 cls
-echo --------------------------------------------------
-echo  Hello and welcome so this program is rewriten
-echo  in python and no longer works the normal 
-echo  batch please migrate to new python based app
-echo  and there will be a legacy branch for archive.  
-echo ---------------------------------------------------
+echo +-------------------------------------------------- +
+echo +  Hello and welcome so this program is rewriten    +
+echo +  in python and no longer works the normal         +
+echo +  batch please migrate to new python based app     +
+echo +  and there will be a legacy branch for archive.   +
+echo +---------------------------------------------------+
 pause
 exit
